be9e3009ca134fad3bccb1a9308256618df9bf6b
z h <zhanif02@gmail.com>
2024-08-30 18:07:55 +0100

    movies added dynamically

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,20 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./TitleCards.css";
 import cards_data from "../../assets/cards/Cards_data";
 
 const TitleCards = ({title, category}) => {
-  const cardsRef = useRef();
+const [apiData, setApiData] = useState([]);
+const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MzkzYzc4ZGYzYmYwM2JmNTEwNjMyOTFjNmM4MzE4MSIsIm5iZiI6MTcyNTAyOTUzMC45Mjg0MjQsInN1YiI6IjY2ZDFkYjczY2FlMjU5YTM0Mjc4YjQwOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bJUzkPWrqVdwUt39P7zEyhAi3K29zPns3sdHp8pNpts'
+    }
+  };
+  
+  
 
   const handleWheel = (event) => {
     event.preventDefault();
@@ -11,6 +22,11 @@ const TitleCards = ({title, category}) => {
   };
 
   useEffect(() => {
+
+    fetch(`https://api.themoviedb.org/3/movie/${category?category :"now_playing"}?language=en-US&page=1`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results))
+    .catch(err => console.error(err));
     cardsRef.current.addEventListener("wheel", handleWheel);
   }, []);
 
@@ -18,11 +34,11 @@ const TitleCards = ({title, category}) => {
     <div className="title-cards">
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return (
             <div className="card" key={index}>
-              <img src={card.image} alt="" />
-              <p>{card.name}</p>
+              <img src={`https://image.tmdb.org/t/p/w500` +card.backdrop_path} alt="" />
+              <p>{card.original_title}</p>
             </div>
           );
         })}
