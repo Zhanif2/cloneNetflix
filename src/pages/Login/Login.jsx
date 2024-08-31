6fdece0edd34cd765a994881bc2ebcf012c12795
z h <zhanif02@gmail.com>
2024-08-31 18:31:15 +0100

    loading state added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,23 +2,31 @@ import React, { useState } from "react";
 import "./Login.css";
 import logo from "../../assets/logo.png";
 import { login, signup } from "../../firebasee";
+import netflix_spinner from "../../assets/netflix_spinner.gif";
 
 const Login = () => {
   const [signState, setSignState] = useState("Sign In");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-const user_auth = async (event) =>{
-  event.preventDefault();
-  if (signState === "Sign In"){
-    await login (email, password);
-  } else {
-    await signup(name, email, password);
-  }
-}
+  const user_auth = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    if (signState === "Sign In") {
+      await login(email, password);
+    } else {
+      await signup(name, email, password);
+    }
+    setLoading(false);
+  };
 
-  return (
+  return loading ? (
+    <div className="login-spinner">
+      <img src={netflix_spinner} alt="" />
+    </div>
+  ) : (
     <div className="login">
       <img src={logo} alt="" className="login-logo" />
       <div className="login-form">
@@ -53,7 +61,9 @@ const user_auth = async (event) =>{
             type="password"
             placeholder="Password"
           />
-          <button onClick={user_auth} type="submit">{signState} </button>
+          <button onClick={user_auth} type="submit">
+            {signState}{" "}
+          </button>
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" />
