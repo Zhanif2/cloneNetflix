7623ec2420bee0a1994555fbef1817667b32552d
z h <zhanif02@gmail.com>
2024-08-31 18:04:50 +0100

    firebase auth done and login functionality

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,22 @@
 import React, { useState } from "react";
 import "./Login.css";
 import logo from "../../assets/logo.png";
+import { login, signup } from "../../firebasee";
 
 const Login = () => {
   const [signState, setSignState] = useState("Sign In");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+const user_auth = async (event) =>{
+  event.preventDefault();
+  if (signState === "Sign In"){
+    await login (email, password);
+  } else {
+    await signup(name, email, password);
+  }
+}
 
   return (
     <div className="login">
@@ -12,14 +25,35 @@ const Login = () => {
         <h1>{signState}</h1>
         <form>
           {signState === "Sign Up" ? (
-            <input type="text" placeholder="Your name" />
+            <input
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              type="text"
+              placeholder="Your name"
+            />
           ) : (
             <></>
           )}
 
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <button>{signState} </button>
+          <input
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            type="email"
+            placeholder="Email"
+          />
+          <input
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            placeholder="Password"
+          />
+          <button onClick={user_auth} type="submit">{signState} </button>
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" />
@@ -31,11 +65,25 @@ const Login = () => {
         <div className="form-switch">
           {signState === "Sign In" ? (
             <p>
-              New to Netflix? <span onClick={() => {setSignState("Sign Up")}}>Sign Up Now</span>
+              New to Netflix?{" "}
+              <span
+                onClick={() => {
+                  setSignState("Sign Up");
+                }}
+              >
+                Sign Up Now
+              </span>
             </p>
           ) : (
             <p>
-              Already have an account? <span onClick={() => {setSignState("Sign In")}}>Sign In Now</span>
+              Already have an account?{" "}
+              <span
+                onClick={() => {
+                  setSignState("Sign In");
+                }}
+              >
+                Sign In Now
+              </span>
             </p>
           )}
         </div>
